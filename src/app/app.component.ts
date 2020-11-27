d297217a278011836a4afa9d80dc320e00e38fe3
Victor Alberto Ramirez Chavez <fdhior@gmail.com>
2020-11-27 11:15:07 -0600

    Directivas y otros cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Platzisquare';
-  lugares: any;
+  lugares: any = [
+    {cercania: 1, distancia: 1, active: true, nombre: 'Floreria La Gadenía'},
+    {cercania: 1, distancia: 1.8, active: true, nombre: 'Donas la pasadita'},
+    {cercania: 2, distancia: 5, active: true, nombre: 'Veterinaria Huellitas Felices'},
+    {cercania: 3, distancia: 10, active: false, nombre: 'Sushi Sushiroll'},
+    {cercania: 3, distancia: 35, active: true, nombre: 'Hotel la Gracia'},
+    {cercania: 3, distancia: 120, active: false, nombre: 'Zapatería el Clavo'}
+  ];
+
+  lat: number = 51.5007325;
+  lng: number = -0.1268141;
+
 
   /* a = 3;
   b = 5;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,10 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCYuuwMJckphtHiPByphNT565rKLXTM92Q'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
